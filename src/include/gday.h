3a6375133b321dceefd2ec012186902986f5bafa
mdekauwe <mdekauwe@gmail.com>
2015-02-28 00:32:20 +1100

    Added all of the param comments

--- src/include/gday.h
+++ src/include/gday.h
@@ -187,8 +187,8 @@ typedef struct {
 } state;
 
 typedef struct {
-    double actncmax;
-    double actncmin;
+    double actncmax;                        /* Active pool (=1/3) N:C ratio of new SOM - maximum [units: gN/gC]. Based on forest version of CENTURY (Parton et al. 1993), see Appendix, McMurtrie 2001, Tree Physiology. */
+    double actncmin;                        /* Active pool (=1/15) N:C of new SOM - when Nmin=Nmin0 [units: gN/gC]. Based on forest version of CENTURY (Parton et al. 1993), see Appendix, McMurtrie 2001, Tree Physiology. */
     double adapt;
     double ageold;                          /* Plant age when max leaf N C ratio is lowest */
     double ageyoung;                        /* Plant age when max leaf N C ratio is highest */
@@ -197,10 +197,10 @@ typedef struct {
     double alpha_j;                         /* initial slope of rate of electron transport, used in calculation of quantum yield. Value calculated by Belinda */
     double b_root;
     double b_topsoil;
-    double bdecay;
-    double branch0;
-    double branch1;
-    double bretrans;
+    double bdecay;                          /* branch and large root turnover rate (1/yr) */
+    double branch0;                         /* constant in branch-stem allometry (trees) */
+    double branch1;                         /* exponent in branch-stem allometry */
+    double bretrans;                        /* branch n retranslocation fraction */
     double c_alloc_bmax;                    /* allocation to branches at branch n_crit. If using allometric model this is the max alloc to branches */
     double c_alloc_bmin;                    /* allocation to branches at zero branch n/c. If using allometric model this is the min alloc to branches */
     double c_alloc_cmax;                    /* allocation to coarse roots at n_crit. If using allometric model this is the max alloc to coarse roots */
@@ -209,18 +209,18 @@ typedef struct {
     double c_alloc_rmax;                    /* allocation to roots at root n_crit. If using allometric model this is the max alloc to fine roots */
     double c_alloc_rmin;                    /* allocation to roots at zero root n/c. If using allometric model this is the min alloc to fine roots */
     double cfracts;                         /* carbon fraction of dry biomass */
-    double crdecay;
-    double cretrans;
-    double croot0;
-    double croot1;
+    double crdecay;                         /* coarse roots turnover rate (1/yr) */
+    double cretrans;                        /* coarse root n retranslocation fraction */
+    double croot0;                          /* constant in coarse_root-stem allometry (trees) */
+    double croot1;                          /* exponent in coarse_root-stem allometry */
     double ctheta_root;                     /* Fitted parameter based on Landsberg and Waring */
     double ctheta_topsoil;                  /* Fitted parameter based on Landsberg and Waring */
     double cue;                             /* carbon use efficiency, or the ratio of NPP to GPP */
     double d0;
-    double d0x;
+    double d0x;                             /* Length scale for exponential decline of Umax(z) */
     double d1;
     double delsj;                           /* Deactivation energy for electron transport (J mol-1 k-1) */
-    double density;
+    double density;                         /* sapwood density kg DM m-3 (trees) */
     double direct_frac;                     /* direct beam fraction of incident radiation - this is only used with the BEWDY model */
     double displace_ratio;                  /* Value for coniferous forest (0.78) from Jarvis et al 1976, taken from Jones 1992 pg 67. More standard assumption is 2/3 */
     double disturbance_doy;
@@ -232,16 +232,16 @@ typedef struct {
     double eav;                             /* Activation energy for Rubisco (J mol-1) */
     double edj;                             /* Deactivation energy for electron transport (J mol-1) */
     double faecescn;
-    double faecesn;
-    double fdecay;
-    double fdecaydry;
-    double fhw;
+    double faecesn;                         /* Faeces C:N ratio */
+    double fdecay;                          /* foliage turnover rate (1/yr) */
+    double fdecaydry;                       /* Foliage turnover rate - dry soil (1/yr) */
+    double fhw;                             /* n:c ratio of stemwood - immobile pool and new ring */
     double finesoil;                        /* clay+silt fraction */
-    double fracfaeces;
-    double fracteaten;
-    double fractosoil;
+    double fracfaeces;                      /* Fractn of grazd C that ends up in faeces (0..1) */
+    double fracteaten;                      /* Fractn of leaf prodn eaten by grazers */
+    double fractosoil;                      /* Fractn of grazed N recycled to soil:faeces+urine */
     double fractup_soil;                    /* fraction of uptake from top soil layer */
-    double fretrans;
+    double fretrans;                        /* foliage n retranslocation fraction - 46-57% in young E. globulus trees - see Corbeels et al 2005 ecological modelling 187, pg 463. Roughly 50% from review Aerts '96 */
     double g1;                              /* stomatal conductance parameter: Slope of reln btw gs and assimilation (fitted by species/pft). */
     double gamstar25;                       /* Base rate of CO2 compensation point at 25 deg C [umol mol-1] */
     double growth_efficiency;               /* growth efficiency (yg) - used only in Bewdy */
@@ -258,54 +258,54 @@ typedef struct {
     double jv_intercept;                    /* Jmax to Vcmax intercept */
     double jv_slope;                        /* Jmax to Vcmax slope */
     double kc25;                            /* Base rate for carboxylation by Rubisco at 25degC [mmol mol-1] */
-    double kdec1;
-    double kdec2;
-    double kdec3;
-    double kdec4;
-    double kdec5;
-    double kdec6;
-    double kdec7;
+    double kdec1;                           /* surface structural decay rate (1/yr) */
+    double kdec2;                           /* surface metabolic decay rate (1/yr) */
+    double kdec3;                           /* soil structural decay rate (1/yr) */
+    double kdec4;                           /* soil metabolic decay rate(1/yr) */
+    double kdec5;                           /* active pool decay rate (1/yr) */
+    double kdec6;                           /* slow pool decay rate (1/yr) */
+    double kdec7;                           /* passive pool decay rate (1/yr) */
     double kext;                            /* extinction coefficient */
     double knl;
     double ko25;                            /* Base rate for oxygenation by Rubisco at 25degC [umol mol-1]. Note value in Bernacchie 2001 is in mmol!! */
     double kq10;                            /* exponential coefficient for Rm vs T */
-    double kr;
+    double kr;                              /* N uptake coefficent (0.05 kg C m-2 to 0.5 tonnes/ha) see Silvia's PhD, Dewar and McM, 96. */
     double lai_closed;                      /* LAI of closed canopy (max cover fraction is reached (m2 (leaf) m-2 (ground) ~ 2.5) */
     double latitude;                        /* latitude (degrees, negative for south) */
     double leafsap0;                        /* leaf area  to sapwood cross sectional area ratio when Height = Height0 (mm^2/mm^2) */
     double leafsap1;                        /* leaf to sap area ratio when Height = Height1 (mm^2/mm^2) */
-    double ligfaeces;
-    double ligroot;
-    double ligshoot;
+    double ligfaeces;                       /* Faeces lignin as fractn of biomass */
+    double ligroot;                         /* lignin-to-biomass ratio in root litter; Values from White et al. = 0.22  - Value in Smith et al. 2013 = 0.16, note subtly difference in eqn C9. */
+    double ligshoot;                        /* lignin-to-biomass ratio in leaf litter; Values from White et al. DBF = 0.18; ENF = 0.24l; GRASS = 0.09; Shrub = 0.15 - Value in smith et al. 2013 = 0.2, note subtly difference in eqn C9. */
     double liteffnc;
     double max_intercep_lai;                /* canopy LAI at which interception is maximised. */
     double measurement_temp;                /* temperature Vcmax/Jmax are measured at, typical 25.0 (celsius)  */
-    double ncbnew;
-    double ncbnewz;
-    double nccnew;
-    double nccnewz;
-    double ncmaxfold;
-    double ncmaxfyoung;
-    double ncmaxr;
-    double ncrfac;
-    double ncwimm;
-    double ncwimmz;
-    double ncwnew;
-    double ncwnewz;
-    double nf_crit;
-    double nf_min;
+    double ncbnew;                          /* N alloc param: new branch N C at critical leaf N C */
+    double ncbnewz;                         /* N alloc param: new branch N C at zero leaf N C */
+    double nccnew;                          /* N alloc param: new coarse root N C at critical leaf N C */
+    double nccnewz;                         /* N alloc param: new coarse root N C at zero leaf N C */
+    double ncmaxfold;                       /* max N:C ratio of foliage in old stand, if the same as young=no effect */
+    double ncmaxfyoung;                     /* max N:C ratio of foliage in young stand, if the same as old=no effect */
+    double ncmaxr;                          /* max N:C ratio of roots */
+    double ncrfac;                          /* N:C of fine root prodn / N:C c of leaf prodn */
+    double ncwimm;                          /* N alloc param: Immobile stem N C at critical leaf N C */
+    double ncwimmz;                         /* N alloc param: Immobile stem N C at zero leaf N C */
+    double ncwnew;                          /* N alloc param: New stem ring N:C at critical leaf N:C (mob) */
+    double ncwnewz;                         /* N alloc param: New stem ring N:C at zero leaf N:C (mobile) */
+    double nf_crit;                         /* leaf N:C below which N availability limits productivity  */
+    double nf_min;                          /* leaf N:C minimum N concentration which allows productivity */
     double nmax;
-    double nmin;
-    double nmin0;
-    double nmincrit;
+    double nmin;                            /* (bewdy) minimum leaf n for +ve p/s (g/m2) */
+    double nmin0;                           /* mineral N pool corresponding to Actnc0,etc (g/m2) */
+    double nmincrit;                        /* Critical mineral N pool at max soil N:C (g/m2) (Parton et al 1993, McMurtrie et al 2001). */
     double ntheta_root;                     /* Fitted parameter based on Landsberg and Waring */
     double ntheta_topsoil;                  /* Fitted parameter based on Landsberg and Waring */
-    double nuptakez;
+    double nuptakez;                        /* constant N uptake per year (1/yr) */
     double oi;                              /* intercellular concentration of O2 [umol mol-1] */
     double passivesoilnz;
     double passivesoilz;
-    double passncmax;
-    double passncmin;
+    double passncmax;                       /* Passive pool (=1/7) N:C ratio of new SOM - maximum [units: gN/gC]. Based on forest version of CENTURY (Parton et al. 1993), see Appendix, McMurtrie 2001, Tree Physiology. */
+    double passncmin;                       /* Passive pool (=1/10) N:C of new SOM - when Nmin=Nmin0 [units: gN/gC]. Based on forest version of CENTURY (Parton et al. 1993), see Appendix, McMurtrie 2001, Tree Physiology. */
     double prescribed_leaf_NC;              /* If the N-Cycle is switched off this needs to be set, e.g. 0.03 */
     double previous_ncd;                    /* In the first year we don't have last years data, so I have precalculated the average of all the november-jan chilling values  */
     double prime_y;
@@ -313,28 +313,28 @@ typedef struct {
     double psi_sat_root;                    /* MPa */
     double psi_sat_topsoil;                 /* MPa */
     double qs;                              /* exponent in water stress modifier, =1.0 JULES type representation, the smaller the values the more curved the depletion.  */
-    double r0;
-    double rateloss;
-    double rateuptake;
-    double rdecay;
-    double rdecaydry;
-    double retransmob;
+    double r0;                              /* root C at half-maximum N uptake (kg C/m3) */
+    double rateloss;                        /* Rate of N loss from mineral N pool (/yr) */
+    double rateuptake;                      /* ate of N uptake from mineral N pool (/yr) from here? http://face.ornl.gov/Finzi-PNAS.pdf */
+    double rdecay;                          /* root turnover rate (1/yr) */
+    double rdecaydry;                       /* root turnover rate - dry soil (1/yr) */
+    double retransmob;                      /* Fraction stem mobile N retranscd (/yr) */
     double return_interval;
     double rfmult;
     double root_exu_CUE;
     double rooting_depth;                   /* Rooting depth (mm) */
     char   rootsoil_type[STRING_LENGTH];
-    double rretrans;
-    double sapturnover;
+    double rretrans;                        /* root n retranslocation fraction */
+    double sapturnover;                     /* Sapwood turnover rate: conversion of sapwood to heartwood (1/yr) */
     double sla;                             /* specific leaf area (m2 one-sided/kg DW) */
     double slamax;                          /* (if equal slazero=no effect) specific leaf area new fol at max leaf N/C (m2 one-sided/kg DW) */
     double slazero;                         /* (if equal slamax=no effect) specific leaf area new fol at zero leaf N/C (m2 one-sided/kg DW) */
-    double slowncmax;
-    double slowncmin;
+    double slowncmax;                       /* Slow pool (=1/15) N:C ratio of new SOM - maximum [units: gN/gC]. Based on forest version of CENTURY (Parton et al. 1993), see Appendix, McMurtrie 2001, Tree Physiology. */
+    double slowncmin;                       /* Slow pool (=1/40) N:C of new SOM - when Nmin=Nmin0" [units: gN/gC]. Based on forest version of CENTURY (Parton et al. 1993), see Appendix, McMurtrie 2001, Tree Physiology. */
     double store_transfer_len;
-    double structcn;
-    double structrat;
-    double targ_sens;
+    double structcn;                        /* C:N ratio of structural bit of litter input */
+    double structrat;                       /* structural input n:c as fraction of metab */
+    double targ_sens;                       /* sensitivity of allocation (leaf/branch) to track the target, higher values = less responsive. */
     double theta;                           /* curvature of photosynthetic light response curve */
     double theta_sat_root;
     double theta_sat_topsoil;
@@ -343,13 +343,13 @@ typedef struct {
     double vcmax;                           /* maximum rate of carboxylation (umol m-2 s-1)  */
     double vcmaxna;                         /* slope of the reln btween vcmax and leaf N content, units = (umol [gN]-1 s-1) # And for Vcmax-N slopes (vcmaxna) see Table 8.2 in CLM4_tech_note, Oleson et al. 2010. */
     double vcmaxnb;                         /* intercept of vcmax vs n, units = (umol [gN]-1 s-1) # And for Vcmax-N slopes (vcmaxna) see Table 8.2 in CLM4_tech_note, Oleson et al. 2010. */
-    double watdecaydry;
-    double watdecaywet;
+    double watdecaydry;                     /* water fractn for dry litterfall rates */
+    double watdecaywet;                     /* water fractn for wet litterfall rates */
     double wcapac_root;                     /* Max plant avail soil water -root zone, i.e. total (mm) (smc_sat-smc_wilt) * root_depth (750mm) = [mm (water) / m (soil depth)] */
     double wcapac_topsoil;                  /* Max plant avail soil water -top soil (mm) */
-    double wdecay;
+    double wdecay;                          /* wood turnover rate (1/yr) */
     double wetloss;                         /* Daily rainfall lost per lai (mm/day) */
-    double wretrans;
+    double wretrans;                        /* mobile wood N retranslocation fraction */
     double z0h_z0m;                         /* Assume z0m = z0h, probably a big assumption [as z0h often < z0m.], see comment in code!! But 0.1 might be a better assumption */
     double fmleaf;
     double fmroot;
