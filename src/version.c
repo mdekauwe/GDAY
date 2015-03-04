6cbd74f3a21e22247d4d689c95969ae3e026a16f
mdekauwe <mdekauwe@gmail.com>
2015-03-04 23:52:32 +1100

    Added exudation/priming funcs

--- src/version.c
+++ src/version.c
@@ -1,3 +1,3 @@
 #include "version.h"
-const char *build_git_sha = "3a77c62bb23240a58fc67192ee4741eb4fb07bac";
-const char *build_git_time = "Wed  4 Mar 2015 23:32:46 AEDT";
+const char *build_git_sha = "2f77f35cc6fd0d71e2a0c56c4636fc477b5ad111";
+const char *build_git_time = "Wed  4 Mar 2015 23:52:11 AEDT";
