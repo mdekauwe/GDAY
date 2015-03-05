d8fdb82054376a7fa2b1f4cc851ca06bcfb486f7
mdekauwe <mdekauwe@gmail.com>
2015-03-05 12:41:05 +1100

    changed if logic

--- src/version.c
+++ src/version.c
@@ -1,3 +1,3 @@
 #include "version.h"
-const char *build_git_sha = "55152edfb09913cf5ae1ca9f3389d00373ebcbdc";
-const char *build_git_time = "Thu  5 Mar 2015 11:56:22 AEDT";
+const char *build_git_sha = "035040cb67d9e0a7842ce992c3207582183b30b7";
+const char *build_git_time = "Thu  5 Mar 2015 12:39:19 AEDT";
