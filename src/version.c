9d3eb06828aa11b1241bccc45d478f949a703cb6
mdekauwe <mdekauwe@gmail.com>
2015-03-17 22:26:13 +1100

    updated gitignore

--- src/version.c
+++ src/version.c
@@ -1,3 +1,3 @@
 #include "version.h"
-const char *build_git_sha = "0f19e243c888f31855dd4571879f9a0f98ea12d0";
-const char *build_git_time = "Tue 17 Mar 2015 21:22:47 AEDT";
+const char *build_git_sha = "000f7f3de9532f4ac13452ce513001c7d1e0bcd3";
+const char *build_git_time = "Tue 17 Mar 2015 22:25:10 AEDT";
