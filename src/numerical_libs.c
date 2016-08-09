2d377ef85c06d55bb3466c5eb6c6d7b1555ab0f7
mdekauwe <mdekauwe@gmail.com>
2016-08-09 13:54:17 +1000

    Fixed spacing in zbrent

--- src/numerical_libs.c
+++ src/numerical_libs.c
@@ -15,71 +15,71 @@ double zbrent(double (*func)(double, double, double, double, double, double),
     ** Numerical Recipies in C, chapter 9.3
     */
     int    iter;
-	double a=x1,b=x2,c,d,e,min1,min2;
-	double fa=(*func)(a, root_biomass, surf_biomass, rooted_layers,
-               top_lyr_thickness, root_reach);
+    double a=x1,b=x2,c,d,e,min1,min2;
+    double fa=(*func)(a, root_biomass, surf_biomass, rooted_layers,
+                      top_lyr_thickness, root_reach);
     double fb=(*func)(b, root_biomass, surf_biomass, rooted_layers,
-               top_lyr_thickness, root_reach);
+                      top_lyr_thickness, root_reach);
     double fc,p,q,r,s,tol1,xm;
-    
-	if (fb*fa > 0.0) {
-	    printf("ERROR: Root must be bracketed in ZBRENT\n");
-	    exit(EXIT_FAILURE);
+
+    if (fb*fa > 0.0) {
+        printf("ERROR: Root must be bracketed in ZBRENT\n");
+        exit(EXIT_FAILURE);
 	}
 	fc=fb;
 	for (iter=1; iter<=ITMAX; iter++) {
-		if (fb*fc > 0.0) {
-			c=a;
-			fc=fa;
-			e=d=b-a;
-		}
-		if (fabs(fc) < fabs(fb)) {
-			a=b;
-			b=c;
-			c=a;
-			fa=fb;
-			fb=fc;
-			fc=fa;
-		}
-		tol1=2.0*EPS*fabs(b)+0.5*tol;
-		xm=0.5*(c-b);
-		if (fabs(xm) <= tol1 || fb == 0.0)
+        if (fb*fc > 0.0) {
+            c=a;
+            fc=fa;
+            e=d=b-a;
+        }
+        if (fabs(fc) < fabs(fb)) {
+            a=b;
+            b=c;
+            c=a;
+            fa=fb;
+            fb=fc;
+            fc=fa;
+        }
+    	tol1=2.0*EPS*fabs(b)+0.5*tol;
+    	xm=0.5*(c-b);
+    	if (fabs(xm) <= tol1 || fb == 0.0)
             return b;
-		if (fabs(e) >= tol1 && fabs(fa) > fabs(fb)) {
-			s=fb/fa;
-			if (a == c) {
-				p=2.0*xm*s;
-				q=1.0-s;
-			} else {
-				q=fa/fc;
-				r=fb/fc;
-				p=s*(2.0*xm*q*(q-r)-(b-a)*(r-1.0));
-				q=(q-1.0)*(r-1.0)*(s-1.0);
-			}
-			if (p > 0.0)  q = -q;
-			p=fabs(p);
-			min1=3.0*xm*q-fabs(tol1*q);
-			min2=fabs(e*q);
-			if (2.0*p < (min1 < min2 ? min1 : min2)) {
-				e=d;
-				d=p/q;
-			} else {
-				d=xm;
-				e=d;
-			}
-		} else {
-			d=xm;
-			e=d;
-		}
-		a=b;
-		fa=fb;
-		if (fabs(d) > tol1)
-			b += d;
-		else
-			b += (xm > 0.0 ? fabs(tol1) : -fabs(tol1));
-		fb=(*func)(b, root_biomass, surf_biomass, rooted_layers,
+    	if (fabs(e) >= tol1 && fabs(fa) > fabs(fb)) {
+            s=fb/fa;
+            if (a == c) {
+                p=2.0*xm*s;
+                q=1.0-s;
+            } else {
+                q=fa/fc;
+                r=fb/fc;
+                p=s*(2.0*xm*q*(q-r)-(b-a)*(r-1.0));
+                q=(q-1.0)*(r-1.0)*(s-1.0);
+            }
+            if (p > 0.0)  q = -q;
+            p=fabs(p);
+            min1=3.0*xm*q-fabs(tol1*q);
+            min2=fabs(e*q);
+            if (2.0*p < (min1 < min2 ? min1 : min2)) {
+                e=d;
+                d=p/q;
+    		} else {
+                d=xm;
+                e=d;
+    		}
+    	} else {
+            d=xm;
+            e=d;
+    	}
+    	a=b;
+    	fa=fb;
+    	if (fabs(d) > tol1)
+            b += d;
+    	else
+            b += (xm > 0.0 ? fabs(tol1) : -fabs(tol1));
+    	fb=(*func)(b, root_biomass, surf_biomass, rooted_layers,
                    top_lyr_thickness, root_reach);
-	}
+    }
 
     printf("Maximum number of iterations exceeded in ZBRENT\n");
 	exit(EXIT_FAILURE);
