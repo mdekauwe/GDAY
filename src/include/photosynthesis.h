8e57413b09cca2e03ca9ab02a1fcfcef28df97c4
mdekauwe <mdekauwe@gmail.com>
2016-12-14 12:11:40 +1100

    Fixed spaces in photosynthesis.h

--- src/include/photosynthesis.h
+++ src/include/photosynthesis.h
@@ -14,7 +14,7 @@ double calculate_michaelis_menten(params *, double);
 void   calculate_jmaxt_vcmaxt(control *, canopy_wk *, params *, state *,
                               double, double *, double *);
 void   calculate_jmaxt_vcmaxt_with_p(control *, canopy_wk *, params *, state *,
-                              double, double *, double *);
+                                     double, double *, double *);
 double arrhenius(double, double, double, double);
 double peaked_arrhenius(double, double, double, double, double, double);
 double calc_leaf_day_respiration(double, double);
@@ -32,7 +32,7 @@ double  arrh(double, double, double, double);
 double  peaked_arrh(double, double, double, double, double, double);
 double  calculate_michaelis_menten_parameter(params *, double, double);
 void    calculate_jmax_and_vcmax(control *, params *, state *, double, double,
-                                        double *, double *, double);
+                                 double *, double *, double);
 void    calculate_jmax_and_vcmax_with_p(control *, params *, state *, double, double,double,
                                         double *, double *, double);
 void    adj_for_low_temp(double *, double);
@@ -46,7 +46,7 @@ double  epsilon(params *, double, double, double, double);
 /* C4 additional prototypes */
 void   mate_C4_photosynthesis(control *, fluxes *, met *, params *,
                               state *, double, double, double);
-void   calculate_vcmax_parameter(params *, state *s, double, double, 
+void   calculate_vcmax_parameter(params *, state *s, double, double,
                                  double *,
                                  double *, double);
 double calc_respiration(double, double);
