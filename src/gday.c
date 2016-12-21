6767a839927c36625422f3064a4f4e7586e78af0
mdekauwe <mdekauwe@gmail.com>
2016-12-22 01:34:12 +1100

    added debug statements

--- src/gday.c
+++ src/gday.c
@@ -600,7 +600,7 @@ void run_sim(canopy_wk *cw, control *c, fluxes *f, met_arrays *ma, met *m,
 	        exit(EXIT_FAILURE);
         }
     }
-
+    printf("Fine to here\n");
     if (odata != NULL) {
         free(odata);
     }
@@ -655,8 +655,8 @@ void spin_up_pools(canopy_wk *cw, control *c, fluxes *f, met_arrays *ma, met *m,
             }
 
             /* Have we reached a steady state? */
-            fprintf(stderr,
-              "Spinup: Plant C - %f, Soil C - %f\n", s->plantc, s->soilc);
+            //fprintf(stderr,
+            //  "Spinup: Plant C - %f, Soil C - %f\n", s->plantc, s->soilc);
         }
     }
     write_final_state(c, p, s);
