ba5873eddf64b4eceb9a88830748785ec141c176
mdekauwe <mdekauwe@gmail.com>
2017-03-30 11:45:10 +1100

    code cleanup

--- src/water_balance_sub_daily.c
+++ src/water_balance_sub_daily.c
@@ -1058,7 +1058,6 @@ double calc_relative_weibull(double p, double p50, double sx) {
     //
     double v, relative_weibull;
 
-    // parameterized as in Ogle et al.
     v = -50.0 * log(0.5);
     relative_weibull = 1.0 - pow(0.5, pow((p / p50), (p50 * sx) / v));
 
